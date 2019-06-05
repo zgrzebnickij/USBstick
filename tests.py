12712277a610d1a02908625da6ce79da9f6d6a3a
zgrzebnickij <jakub.zgrzeb@gmail.com>
2019-06-05 22:39:41 +0200

    add test if there are two answers

--- tests.py
+++ tests.py
@@ -69,6 +69,29 @@ class TestExampleData(unittest.TestCase):
                          results,
                          "Calculate function do not return empty set")
 
+    def test_calculate_with_two_answers(self):
+        usb_size = 1
+        memes1 = [
+            ('rollsafe.jpg', 400, 6),
+            ('sad_pepe_compilation.gif', 400, 6),
+            ('yodeling.avi', 800, 12),
+        ]
+        memes2 = [
+            ('yodeling.avi', 800, 12),
+            ('rollsafe.jpg', 400, 6),
+            ('sad_pepe_compilation.gif', 400, 6),
+        ]
+
+        results1 = calculate(usb_size, memes1)
+        results2 = calculate(usb_size, memes2)
+        print(results1, results2)
+        self.assertEqual(results1[0],
+                         results2[0],
+                         "Calculate function do not give the same price")
+        self.assertNotEqual(results1[1],
+                            results2[1],
+                            "Calculate function do not depend on memes order")
+
 
 if __name__ == '__main__':
     unittest.main()
